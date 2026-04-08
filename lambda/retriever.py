74ab6c2d00ec30ddd761f98de5b7bc60527efa1e
veronicasalt <veronica49saltanov@gmail.com>
2026-04-07 22:10:26 -0700

    implement TTL in retriever.py

--- lambda/retriever.py
+++ lambda/retriever.py
@@ -3,10 +3,30 @@ import os
 import boto3
 from decimal import Decimal
 
-
 dynamodb = boto3.resource('dynamodb')
 
+cache = {
+    "data": None,
+    "expiry": 0
+}
+TTL_SECONDS = 3600 # 1 hour
+
 def handler(event, context):
+    global cache
+    current_time = time.time()
+
+    if cache["data"] and current_time < cache["expiry"]:
+        print("Returning cached data...")
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, OPTIONS" 
+            },
+            "body": json.dumps(cache["data"], default=str)
+        }
+
     table_name = os.environ.get('TABLE_NAME')
     table = dynamodb.Table(table_name)
 
